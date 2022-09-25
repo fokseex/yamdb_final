48766ee9f39d8acd1de9c4e27a9cf09f7ed79ea3
Evgeniy Kazakov <junglecommunity@gmail.com>
2022-09-25 14:37:14 +0700

    hide secret key django

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', default=None)
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', default='1234567890')
 
 DEBUG = False
 
